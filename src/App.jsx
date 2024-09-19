0393ef50f1541bbaae29212d9d2bab88b3694924
Andrii D. <and05gt@gmail.com>
2024-09-19 20:56:37 +0200

    Add components in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import "./App.css";
+import ErrorMessage from "./components/ErrorMessage/ErrorMessage";
+import ImageGallery from "./components/ImageGallery/ImageGallery";
+import ImageModal from "./components/ImageModal/ImageModal";
+import Loader from "./components/Loader/Loader";
+import LoadMoreBtn from "./components/LoadMoreBtn/LoadMoreBtn";
+import SearchBar from "./components/SearchBar/SearchBar";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <SearchBar />
+      <ImageGallery />
+      <Loader />
+      <ErrorMessage />
+      <LoadMoreBtn />
+      <ImageModal />
+    </>
+  );
 }
 
 export default App;
